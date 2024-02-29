734368e3b54c49a132aff14985336bf18012a023
Arjun_Negi <156993385+CoderOtakuX@users.noreply.github.com>
2024-02-29 18:00:52 +0530

    Add files via upload

---
+++ music-list.js
@@ -0,0 +1,74 @@
+let allMusic = [
+  {
+    name: "Teri yaadoin mein",
+    artist: "Sushant",
+    img: "Sushi1",
+    src: "Sushi1"
+  },
+  {
+    name: "Phir Kabhi",
+    artist: "Sushant",
+    img: "music-2",
+    src: "Sushi2"
+  },
+  {
+    name: "Labon ko",
+    artist: "Sushant",
+    img: "Sushi3",
+    src: "Sushi3"
+  },
+  {
+    name: "Ab To Aadat Si Hai",
+    artist: "Sushant",
+    img: "Sushi4",
+    src: "Sushi4"
+  },
+  {
+    name: "Why This Kolaveri Di",
+    artist: "Sushant",
+    img: "Sushi5",
+    src: "Sushi5"
+  },
+  {
+    name: "Tum hi ho",
+    artist: "Sushant",
+    img: "Sushi6",
+    src: "Sushi6"
+  },
+  {
+    name: "Apna Phir Milenge",
+    artist: "Sambhav",
+    img: "Sushi7",
+    src: "Sushi7"
+  },
+  {
+    name: "Roke Na Ruke Naina",
+    artist: "Collab",
+    img: "Sushi8",
+    src: "Sushi8"
+  },
+  {
+    name: "Kun faya kun",
+    artist: "Sushant",
+    img: "Sushi9",
+    src: "Sushi9"
+  },
+  {
+    name: "Dil Diyan Galliyan",
+    artist: "Sushant",
+    img: "Sushi10",
+    src: "Sushi10"
+  },
+  {
+    name: "Tune Jo Na Kah",
+    artist: "Sambhav",
+    img: "Sushi12",
+    src: "Sushi12"
+  },
+  {
+    name: "Sun Mere Hamsafar",
+    artist: "Sambhav",
+    img: "Sushi11",
+    src: "Sushi11"
+  }
+];
